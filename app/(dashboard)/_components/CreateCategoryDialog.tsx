0fdf0fa2a30b4b4b7004ae92f3ce788a1618023a
Benjamin Bamisile <bambobtim@gmail.com>
2024-10-25 15:48:39 -0500

    transaction creation

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -32,17 +32,23 @@ import {
   CreateCategorySchemaType,
 } from "@/schema/categories";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CircleOff, PlusSquareIcon } from "lucide-react";
-import React, { useState } from "react";
+import { CircleOff, Loader2, PlusSquareIcon } from "lucide-react";
+import React, { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
-import Picker from "@emoji-mart/react"
+import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CreateCategory } from "../_actions/categories";
+import { Category } from "@prisma/client";
+import { toast } from "sonner";
+import { useTheme } from "next-themes";
 
 type Props = {
   type: TransactionType;
+  successCallback: (category: Category) => void;
 };
 
-function CreateCategoryDialog({ type }: Props) {
+function CreateCategoryDialog({ type, successCallback }: Props) {
   const [open, setOpen] = useState(false);
   const form = useForm<CreateCategorySchemaType>({
     resolver: zodResolver(CreateCategorySchema),
@@ -50,19 +56,62 @@ function CreateCategoryDialog({ type }: Props) {
       type,
     },
   });
+
+  const queryClient = useQueryClient();
+  const theme = useTheme()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateCategory,
+    onSuccess: async (data: Category) => {
+      form.reset({
+        name: "",
+        icon: "",
+        type,
+      });
+
+      toast.success(`Category ${data.name} created successfully 🎉`, {
+        id: "create-category",
+      });
+
+      successCallback(data);
+
+      await queryClient.invalidateQueries({
+        queryKey: ["categories"],
+      });
+
+      setOpen((prev) => !prev);
+    },
+    onError: () => {
+      toast.error("Something went wrong", {
+        id: "create-category",
+      });
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: CreateCategorySchemaType) => {
+      toast.loading("Creating category...", {
+        id: "create-category",
+      });
+      mutate(values);
+    },
+    [mutate]
+  );
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button
-          variant={"ghost"}
-          className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
+          variant="ghost"
+          className="flex items-center justify-start rounded-lg border px-4 py-3 text-muted-foreground hover:bg-muted/50 transition-colors"
         >
           <PlusSquareIcon className="mr-2 h-4 w-4" />
           Create new
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
+
+      <DialogContent className="max-w-md w-full mx-auto">
+        <DialogHeader className="space-y-3 pb-4 border-b">
           <DialogTitle>
             Create{" "}
             <span
@@ -76,22 +125,30 @@ function CreateCategoryDialog({ type }: Props) {
             category
           </DialogTitle>
           <DialogDescription>
-            Categories are used to group your transactions
+            Categories help you organize and track your transactions better
           </DialogDescription>
         </DialogHeader>
+
         <Form {...form}>
-          <form className="space-y-8">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6 py-4"
+          >
             <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel className="text-sm font-medium">Name</FormLabel>
                   <FormControl>
-                    <Input defaultValue={""} {...field} />
+                    <Input
+                      placeholder="Enter category name"
+                      className="w-full"
+                      {...field}
+                    />
                   </FormControl>
-                  <FormDescription>
-                    Transaction description (optional)
+                  <FormDescription className="text-xs text-gray-500">
+                    Choose a clear, descriptive name
                   </FormDescription>
                 </FormItem>
               )}
@@ -102,70 +159,93 @@ function CreateCategoryDialog({ type }: Props) {
               name="icon"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Icon</FormLabel>
-                  <FormControl>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={"outline"}
-                          className="h-[100px] w-full"
-                        >
-                          {form.watch("icon") ? (
-                            <div className="flex flex-col items-center gap-2">
-                              <span className="text-5xl" role="img">
-                                {field.value}
-                              </span>
-
-                              <p className="text-xs text-muted-foreground">
-                                Click to change
-                              </p>
-                            </div>
-                          ) : (
-                            <div className="flex flex-col items-center gap-2">
-                              <CircleOff className="h-[48px] w-[48px]" />
-
-                              <p className="text-xs text-muted-foreground">
-                                Click to select
-                              </p>
-                            </div>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-full ">
-                        <Picker
-                          data={data}
-                          onEmojiSelect={(emoji: { native: string }) => {
-                            field.onChange(emoji.native);
-                          }}
-                        ></Picker>
-                      </PopoverContent>
-                    </Popover>
-                  </FormControl>
-                  <FormDescription>
-                    This is how your category will appear in the app
+                  <FormLabel className="text-sm font-medium">Icon</FormLabel>
+                  <div className="space-y-4">
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <Button
+                            type="button"
+                            variant="outline"
+                            className="h-[100px] w-full hover:bg-muted/50 transition-colors"
+                          >
+                            {field.value ? (
+                              <div className="flex flex-col items-center gap-2">
+                                <span className="text-5xl" role="img">
+                                  {field.value}
+                                </span>
+                                <p className="text-xs text-muted-foreground">
+                                  Click to change
+                                </p>
+                              </div>
+                            ) : (
+                              <div className="flex flex-col items-center gap-2">
+                                <CircleOff className="h-[48px] w-[48px] text-muted-foreground" />
+                                <p className="text-xs text-muted-foreground">
+                                  Click to select
+                                </p>
+                              </div>
+                            )}
+                          </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-full p-0" align="center">
+                          <div className="max-h-[min(370px,60vh)] overflow-y-auto">
+                            <Picker
+                              data={data}
+                              theme={theme.resolvedTheme}
+                              onEmojiSelect={(emoji: { native: string }) => {
+                                field.onChange(emoji.native);
+                              }}
+                            />
+                          </div>
+                        </PopoverContent>
+                      </Popover>
+                    </FormControl>
+                  </div>
+                  <FormDescription className="text-xs text-gray-500">
+                    Choose an emoji that represents this category
                   </FormDescription>
                 </FormItem>
               )}
             />
           </form>
         </Form>
-        <DialogFooter>
+
+        <DialogFooter className="flex items-center justify-end gap-2 pt-4 border-t">
           <DialogClose asChild>
             <Button
               type="button"
-              variant={"secondary"}
-              onClick={() => {
-                form.reset();
-              }}
+              variant="secondary"
+              onClick={() => form.reset()}
+              className="px-6"
             >
               Cancel
             </Button>
           </DialogClose>
-          <Button>Save</Button>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={isPending}
+            className={cn(
+              "px-6",
+              type === "income"
+                ? "bg-emerald-600 hover:bg-emerald-700"
+                : "bg-red-600 hover:bg-red-700",
+              "text-white"
+            )}
+          >
+            {isPending ? (
+              <div className="flex items-center gap-2">
+                <Loader2 className="h-4 w-4 animate-spin" />
+                Creating...
+              </div>
+            ) : (
+              "Create Category"
+            )}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
   );
-}
+};
 
 export default CreateCategoryDialog;
--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -2,7 +2,9 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -13,7 +15,7 @@ import {
   CreateTransactionSchema,
   CreateTransactionSchemaType,
 } from "@/schema/transaction";
-import { ReactNode } from "react";
+import { ReactNode, useCallback, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 type Props = {
@@ -30,9 +32,23 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import CategoryPicker from "./CategoryPicker";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { CalendarIcon, Loader2 } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { CreateTransaction } from "../_actions/transactions";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { DateToUTCDate } from "@/lib/helpers";
 
 function CreateTransactionDialog({ trigger, type }: Props) {
   const form = useForm<CreateTransactionSchemaType>({
@@ -42,12 +58,57 @@ function CreateTransactionDialog({ trigger, type }: Props) {
       date: new Date(),
     },
   });
+  const [open, setOpen] = useState(false);
+
+  const handleCategoryChange = useCallback(
+    (value: string) => {
+      form.setValue("category", value);
+    },
+    [form]
+  );
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateTransaction,
+    onSuccess: () => {
+      toast.success("Transaction createed successfully 🎉", {
+        id: "create-transaction",
+      });
+
+      form.reset({
+        type,
+        description: "",
+        date: new Date(),
+        category: undefined,
+      });
+
+      //Validate overview query to refetch data in the homepage
+      queryClient.invalidateQueries({
+        queryKey: ["overview"],
+      });
+
+      setOpen((prev) => !prev);
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: CreateTransactionSchemaType) => {
+      toast.loading("Creating transaction...", { id: "create-transaction" });
+
+      mutate({
+        ...values,
+        date: DateToUTCDate(values.date),
+      });
+    },
+    [mutate]
+  );
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>
+      <DialogContent className="w-full max-w-lg mx-auto sm:max-w-[425px] md:max-w-[500px] lg:max-w-[600px]">
+        <DialogHeader className="space-y-3 pb-4 border-b">
+          <DialogTitle className="text-2xl font-semibold">
             Create a new{" "}
             <span
               className={cn(
@@ -56,63 +117,153 @@ function CreateTransactionDialog({ trigger, type }: Props) {
               )}
             >
               {type}
-            </span>
+            </span>{" "}
             transaction
           </DialogTitle>
         </DialogHeader>
+
         <Form {...form}>
-          <form className="space-y-4">
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input defaultValue={""} {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Transaction description (optional)
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Amount</FormLabel>
-                  <FormControl>
-                    <Input defaultValue={0} type="number" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Transaction amount (required)
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
-
-            <div className="flex items-center justify-between gap-2">
+          <form
+            className="mt-4 space-y-6"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="space-y-4">
               <FormField
                 control={form.control}
-                name="category"
+                name="description"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Category</FormLabel>
+                    <FormLabel className="text-sm font-medium">
+                      Description
+                    </FormLabel>
                     <FormControl>
-                      <CategoryPicker type={type} {...field}/>
+                      <Input
+                        placeholder="Enter transaction description"
+                        className="w-full"
+                        {...field}
+                      />
                     </FormControl>
-                    <FormDescription>
-                      Select a category for this transaction
+                    <FormDescription className="text-xs text-gray-500">
+                      Transaction description (optional)
                     </FormDescription>
                   </FormItem>
                 )}
               />
+
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-sm font-medium">
+                      Amount
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        placeholder="0.00"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-xs text-gray-500">
+                      Transaction amount (required)
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel className="text-sm font-medium">
+                        Category
+                      </FormLabel>
+                      <FormControl>
+                        <div className="mt-1">
+                          <CategoryPicker
+                            type={type}
+                            onChange={handleCategoryChange}
+                          />
+                        </div>
+                      </FormControl>
+                      <FormDescription className="text-xs text-gray-500 mt-1">
+                        Select a category
+                      </FormDescription>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel className="text-sm font-medium">
+                        Transaction date
+                      </FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant="outline"
+                              className={cn(
+                                "w-full mt-1 pl-3 text-left font-normal",
+                                !field.value && "text-gray-400"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={(value) => {
+                              if (!value) return;
+                              field.onChange(value);
+                            }}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormDescription className="text-xs text-gray-500 mt-1">
+                        Select transaction date
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
             </div>
           </form>
         </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => {
+                form.reset();
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+            {!isPending && "Create"}
+            {isPending && <Loader2 className="animate-spin" />}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
